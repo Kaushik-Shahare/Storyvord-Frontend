c0227068dcce8d2e7fb1b49b86eb68939514b01c
Souvik <souvik@storyvord.com>
2025-02-07 08:28:33 +0530

    update api url

--- src/constant/constant.ts
+++ src/constant/constant.ts
@@ -28,16 +28,16 @@ import { RxActivityLog } from "react-icons/rx";
 export const API_URL = "https://sv-aibackend.azurewebsites.net";
 
 console.log(process.env.NEXT_PUBLIC_APP_ENV);
-export const USER_API =
-  process.env.NEXT_PUBLIC_APP_ENV === "dev"
-    ? "https://api-dev.storyvord.com/api"
-    : "https://api-stage.storyvord.com/api";
-export const NEW_API_URL = USER_API;
-export const NEW_API_URL_V2 = USER_API;
-
-// export const USER_API = "localhost:8000/api"
+// export const USER_API =
+//   process.env.NEXT_PUBLIC_APP_ENV === "dev"
+//     ? "https://api-dev.storyvord.com/api"
+//     : "https://api-stage.storyvord.com/api";
 // export const NEW_API_URL = USER_API;
 // export const NEW_API_URL_V2 = USER_API;
+
+export const USER_API = "http://35.200.152.122:8000/api";
+export const NEW_API_URL = USER_API;
+export const NEW_API_URL_V2 = USER_API;
 
 export const userTypes = [
   { value: "option1", label: "Option 1" },
