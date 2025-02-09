437ba22ba929385369e24201d1db81987a5ac4ee
Souvik <souvik@storyvord.com>
2025-02-09 15:48:30 +0530

    implement new scene design

--- src/components/user-dashboard/project-details/breakdown-more/scenes/Navbar.tsx
+++ src/components/user-dashboard/project-details/breakdown-more/scenes/Navbar.tsx
@@ -6,30 +6,47 @@ import { Button } from "@/components/ui/button";
 type Props = {
   setCreateScenesDialog: (value: boolean) => void;
   setUploadScenesDialog: (value: boolean) => void;
+  selectedScenes: number[];
 };
-const Navbar = ({ setCreateScenesDialog, setUploadScenesDialog }: Props) => {
+const Navbar = ({ setCreateScenesDialog, setUploadScenesDialog, selectedScenes }: Props) => {
   return (
     <header className=" flex justify-between mt-4 ">
-      <ButtonGroup
-        setCreateScenesDialog={setCreateScenesDialog}
-        setUploadScenesDialog={setUploadScenesDialog}
-      />
+      <Button
+        variant="outline"
+        className="flex gap-3 font-semibold rounded-md bg-transparent border border-gray-500"
+      >
+        <Image src="/icons/plus.svg" width={16} height={16} alt="download" />
+        Create Scenes
+      </Button>
       <div className=" flex gap-4">
-        <Button variant="outline" className="flex gap-3 font-semibold border border-gray-500">
+        <Button
+          variant="outline"
+          className="flex gap-3 font-semibold rounded-md bg-transparent border border-gray-500"
+        >
           <Image src="/icons/search.svg" width={16} height={16} alt="download" />
           Search
         </Button>
-        <Button variant="outline" className="flex gap-3 font-semibold border border-gray-500">
+        <Button
+          variant="outline"
+          className="flex gap-3 font-semibold rounded-md bg-transparent border border-gray-500"
+        >
           <Image src="/icons/sort.svg" width={16} height={16} alt="download" />
           Sort By
         </Button>
-        <Button variant="outline" className=" flex gap-3 font-semibold border border-gray-500">
+        <Button
+          variant="outline"
+          className=" flex gap-3 font-semibold rounded-md bg-transparent border border-gray-500"
+        >
           <Image src="/icons/download.svg" width={16} height={16} alt="download" />
           Export
         </Button>
-        <Button variant="outline" className=" flex gap-3 font-semibold border border-gray-500">
+        <Button
+          variant="outline"
+          className=" flex gap-3 font-semibold rounded-md bg-transparent border border-gray-500"
+          disabled={selectedScenes.length === 0}
+        >
           <Image src="/icons/settings.svg" width={16} height={16} alt="download" />
-          Settings
+          Convert to Shots
         </Button>
       </div>
     </header>
